979aead778b2506fc4b02dbb436d9b9d57cc8b39
Kushal Das <mail@kushaldas.in>
2020-11-24 21:45:16 +0530

    The read_list and read_single function are in the module level
    Initially I had them implemented for the TLV structure, which does
    not make any sense.

--- src/tlvs.rs
+++ src/tlvs.rs
@@ -58,113 +58,111 @@ pub fn hex<T: UpperHex>(value: T) -> String {
 
 // I am returning (TLV, Vec<u8>) as I still have to figure if any data left to be read
 // and then read it for the next TLV.
+/// Reads the given data and creates the TLV structure from the same. Also returns back any
+/// extra data still remaining in the input. Used internally in the `read_list` function.
 #[allow(unused_assignments)]
-impl TLV {
-    /// Reads the given data and creates the TLV structure from the same. Also returns back any
-    /// extra data still remaining in the input. Used internally in the `read_list` function.
-    pub fn read_single(orig_data: Vec<u8>, recursive: bool) -> Result<(TLV, Vec<u8>), String> {
-        let mut subs: Vec<TLV> = Vec::new();
-        let data = orig_data.clone();
-        let (t, mut data) = get(data);
-        let mut t: u16 = (t & 0xff) as u16;
-        let mut t2: u16 = 0;
-        let composite: bool = (t & 0x20) == 0x20;
-        if (t & 0x1f) == 0x1f {
-            let p = get(data);
-            t2 = p.0 as u16;
-            data = p.1;
-            t2 = t2 & 0xff;
-            if (t2 & 0x1f) == 0x1f {
-                return Err(String::from("Only two bytes for tags"));
-            }
-            let internal_t: u16 = t << 8;
-            t = internal_t | (t2 & 0x7f);
-        }
-
-        //dbg!(hex(t));
-
+pub fn read_single(orig_data: Vec<u8>, recursive: bool) -> Result<(TLV, Vec<u8>), String> {
+    let mut subs: Vec<TLV> = Vec::new();
+    let data = orig_data.clone();
+    let (t, mut data) = get(data);
+    let mut t: u16 = (t & 0xff) as u16;
+    let mut t2: u16 = 0;
+    let composite: bool = (t & 0x20) == 0x20;
+    if (t & 0x1f) == 0x1f {
         let p = get(data);
-        let mut l: u16 = p.0 as u16;
-        let mut data = p.1;
-        if l == 0x81 {
-            let p = get(data);
-            l = p.0 as u16;
-            data = p.1;
-            l = l & 0xff;
-        } else if l == 0x82 {
-            let p = get(data);
-            l = p.0 as u16;
-            data = p.1;
-            l = l & 0xff;
-            let p = get(data);
-            let second_l = p.0 as u16;
-            data = p.1;
-            let internal_l: u16 = l << 8_u8;
-            l = internal_l | (second_l & 0xff);
-        } else if l >= 0x80 {
-            //dbg!("Inside of the error condition");
-            //dbg!(hex(l));
-            return Err(String::from("Invalid length field!"));
+        t2 = p.0 as u16;
+        data = p.1;
+        t2 = t2 & 0xff;
+        if (t2 & 0x1f) == 0x1f {
+            return Err(String::from("Only two bytes for tags"));
         }
+        let internal_t: u16 = t << 8;
+        t = internal_t | (t2 & 0x7f);
+    }
 
+    //dbg!(hex(t));
+
+    let p = get(data);
+    let mut l: u16 = p.0 as u16;
+    let mut data = p.1;
+    if l == 0x81 {
+        let p = get(data);
+        l = p.0 as u16;
+        data = p.1;
+        l = l & 0xff;
+    } else if l == 0x82 {
+        let p = get(data);
+        l = p.0 as u16;
+        data = p.1;
+        l = l & 0xff;
+        let p = get(data);
+        let second_l = p.0 as u16;
+        data = p.1;
+        let internal_l: u16 = l << 8_u8;
+        l = internal_l | (second_l & 0xff);
+    } else if l >= 0x80 {
+        //dbg!("Inside of the error condition");
         //dbg!(hex(l));
-        let len: usize = l.clone() as usize;
-        //let v = data.drain(0..len).collect();
-        // If it is a composite, we need to pass the full value to the recursive call
-        // else, we should consume the value from the data, and pass the rest.
-        let v = if composite == true {
-            data[..len].iter().cloned().collect()
-        } else {
-            data.drain(0..len).into_iter().collect()
-        };
-
-        // Look at the DO(s) which are marked as C in the section 4.4.1
-        // of the OpenPGP-smart-card-application-3.4.1.pdf
-        if recursive && composite {
-            subs = TLV::read_list(data, true);
-            return Ok((TLV { t, l, v, subs }, vec![]));
-        }
-
-        Ok((TLV { t, l, v, subs }, data))
+        return Err(String::from("Invalid length field!"));
     }
 
-    /// This fundtion should be used to parse the data returned by the smartcard to convert them
-    /// into proper TLV structure.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// let mut f = File::open("./data/capabilities_tlv.binary").expect("no file found");
-    /// let mut buffer: Vec<u8> = Vec::new();
-    /// f.read_to_end(&mut buffer).unwrap();
-    /// let tlvs = TLV::read_list(buffer, true);
-    /// for bigb in big_box {
-    ///     let ts = bigb.subs.clone();
-    ///     for tlv in ts {
-    ///         dbg!(tlvs::hex(tlv.t));
-    ///         dbg!(tlvs::hex(tlv.l));
-    ///         dbg!(tlv.if_recursive());
-    ///     }
-    /// }
-    /// ```
-    ///
-    pub fn read_list(orig_data: Vec<u8>, recursive: bool) -> Vec<TLV> {
-        let mut result: Vec<TLV> = Vec::new();
+    //dbg!(hex(l));
+    let len: usize = l.clone() as usize;
+    //let v = data.drain(0..len).collect();
+    // If it is a composite, we need to pass the full value to the recursive call
+    // else, we should consume the value from the data, and pass the rest.
+    let v = if composite == true {
+        data[..len].iter().cloned().collect()
+    } else {
+        data.drain(0..len).into_iter().collect()
+    };
 
-        let mut data = orig_data.clone();
-        while data.len() > 0 {
-            if data[0] == 0xff || data[0] == 0x00 {
-                // Then we skip the filler byte
-                let p = get(data);
-                data = p.1
-            }
-            // Now we can try to get a TLV
-            let tmp = TLV::read_single(data, recursive).unwrap();
-            data = tmp.1;
-            result.push(tmp.0);
-        }
-        result
+    // Look at the DO(s) which are marked as C in the section 4.4.1
+    // of the OpenPGP-smart-card-application-3.4.1.pdf
+    if recursive && composite {
+        subs = read_list(data, true);
+        return Ok((TLV { t, l, v, subs }, vec![]));
     }
+
+    Ok((TLV { t, l, v, subs }, data))
+}
+
+/// This fundtion should be used to parse the data returned by the smartcard to convert them
+/// into proper TLV structure.
+///
+/// # Example
+///
+/// ```
+/// let mut f = File::open("./data/capabilities_tlv.binary").expect("no file found");
+/// let mut buffer: Vec<u8> = Vec::new();
+/// f.read_to_end(&mut buffer).unwrap();
+/// let tlvs = read_list(buffer, true);
+/// for bigb in big_box {
+///     let ts = bigb.subs.clone();
+///     for tlv in ts {
+///         dbg!(tlvs::hex(tlv.t));
+///         dbg!(tlvs::hex(tlv.l));
+///         dbg!(tlv.if_recursive());
+///     }
+/// }
+/// ```
+///
+pub fn read_list(orig_data: Vec<u8>, recursive: bool) -> Vec<TLV> {
+    let mut result: Vec<TLV> = Vec::new();
+
+    let mut data = orig_data.clone();
+    while data.len() > 0 {
+        if data[0] == 0xff || data[0] == 0x00 {
+            // Then we skip the filler byte
+            let p = get(data);
+            data = p.1
+        }
+        // Now we can try to get a TLV
+        let tmp = read_single(data, recursive).unwrap();
+        data = tmp.1;
+        result.push(tmp.0);
+    }
+    result
 }
 
 #[cfg(test)]
@@ -179,8 +177,9 @@ mod tests {
         let mut f = File::open("./data/capabilities_tlv.binary").expect("no file found");
         let mut buffer: Vec<u8> = Vec::new();
         f.read_to_end(&mut buffer).unwrap();
-        let big_box = TLV::read_list(buffer, true);
+        let big_box = read_list(buffer, true);
         assert_eq!(big_box[0].get_t(), 0x006E);
+        // TODO: fix the test to test the real values
         for bigb in big_box {
             let ts = bigb.subs.clone();
             for tlv in ts {
